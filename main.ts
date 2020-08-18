51410f35f6013b61e591ab51b13ea0c33dcbac01
DImitriKoro <69867146+DImitriKoro@users.noreply.github.com>
2020-08-18 19:13:57 +0100

    add Check Thermometer function

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
 input.onButtonPressed(Button.A, function () {
-    for (let A = 0; A <= 180; A++) {
-        robotbit.Servo(robotbit.Servos.S2, A)
-        robotbit.Servo(robotbit.Servos.S1, A)
-        robotbit.Servo(robotbit.Servos.S3, A)
-        basic.showNumber(A)
-        basic.pause(200)
-        A += 15
-        robotbit.rgb().showColor(neopixel.rgb(randint(0, 255), randint(0, 255), randint(0, 255)))
+    for (let A = 0; A <= 10; A++) {
+        basic.pause(1000)
+        CheckTemperature()
     }
 })
+function CheckTemperature () {
+    Temperature = input.temperature()
+    robotbit.Servo(robotbit.Servos.S1, Temperature + 13)
+    basic.showNumber(input.temperature())
+}
 input.onButtonPressed(Button.AB, function () {
 	
 })
@@ -18,17 +18,16 @@ input.onButtonPressed(Button.B, function () {
         basic.showNumber(A)
         basic.pause(200)
         A += 20
-        range.showRainbow(randint(0, 255), randint(0, 255))
+        robotbit.rgb().showColor(neopixel.rgb(randint(0, 255), randint(0, 255), randint(0, 255)))
     }
 })
-let range: neopixel.Strip = null
+let Temperature = 0
 let A = 4
 robotbit.rgb().showColor(neopixel.colors(NeoPixelColors.Black))
 robotbit.rgb().setBrightness(5)
 robotbit.Servo(robotbit.Servos.S1, A)
 robotbit.Servo(robotbit.Servos.S2, A)
-robotbit.Servo(robotbit.Servos.S3, A)
-range = robotbit.rgb().range(0, 4)
+let range = robotbit.rgb().range(0, 4)
 music.ringTone(0)
 robotbit.rgb().showColor(neopixel.colors(NeoPixelColors.Black))
 basic.showString("Hello boy!")
