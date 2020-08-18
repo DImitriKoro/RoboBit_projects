c8e1c7cbd975c4f82a3845889ffa8b3406c8b050
DImitriKoro <69867146+DImitriKoro@users.noreply.github.com>
2020-08-18 18:53:56 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 input.onButtonPressed(Button.A, function () {
     for (let A = 0; A <= 180; A++) {
         robotbit.Servo(robotbit.Servos.S2, A)
+        robotbit.Servo(robotbit.Servos.S1, A)
+        robotbit.Servo(robotbit.Servos.S3, A)
         basic.showNumber(A)
         basic.pause(200)
         A += 15
         robotbit.rgb().showColor(neopixel.rgb(randint(0, 255), randint(0, 255), randint(0, 255)))
-        range.rotate(1)
-        music.ringTone(A)
     }
 })
 input.onButtonPressed(Button.AB, function () {
@@ -26,6 +26,8 @@ let A = 4
 robotbit.rgb().showColor(neopixel.colors(NeoPixelColors.Black))
 robotbit.rgb().setBrightness(5)
 robotbit.Servo(robotbit.Servos.S1, A)
+robotbit.Servo(robotbit.Servos.S2, A)
+robotbit.Servo(robotbit.Servos.S3, A)
 range = robotbit.rgb().range(0, 4)
 music.ringTone(0)
 robotbit.rgb().showColor(neopixel.colors(NeoPixelColors.Black))
